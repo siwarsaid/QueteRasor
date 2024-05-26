bff36fc830a830dfbb5b6df84cb9f5444c9eef6f
siwar <siwarsaid8@gmail.com>
2024-05-26 23:24:49 +0200

    queteRasor

---
+++ QueteRazor/Controllers/ExpenditureController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QueteRazor.Models;
+
+namespace QueteRazor.Controllers
+{
+    public class ExpenditureController : Controller
+    {
+        // GET: ExpenditureController
+        public ActionResult Index()
+        {
+            return View(GetExpenditureModels());
+        }
+
+        // GET: ExpenditureController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        private IEnumerable<ExpenditureModel> GetExpenditureModels()
+        {
+            List<ExpenditureModel> expenditureModels = new List<ExpenditureModel>();
+            expenditureModels.Add(new ExpenditureModel { Date = DateTime.Now , Name = "Toto" , Price = 100.0});
+            expenditureModels.Add(new ExpenditureModel { Date = DateTime.Now, Name = "Toto 1", Price = 110.0 });
+            expenditureModels.Add(new ExpenditureModel { Date = DateTime.Now, Name = "Toto 2", Price = 120.0 });
+            return expenditureModels;
+        }
+
+       
+    }
+}
---
+++ QueteRazor/Models/ExpenditureModel.cs
@@ -0,0 +1,10 @@
+﻿namespace QueteRazor.Models
+{
+    public class ExpenditureModel
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+
+    }
+}
